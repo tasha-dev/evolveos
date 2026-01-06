666791e854857a0045dcec5d50bc093c6ec6d94a
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2026-01-06 17:02:19 +0330

    added hero section of home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 // Codes by mahdi tasha
 // Importing part
 import Header from "@/component/header";
+import Hero from "@/component/section/home/hero";
 
 // Creating and exporting home page as default
 export default function HomePage() {
   // Returning JSX
   return (
-    <div>
+    <>
       <Header />
-      <h1>Hello world</h1>
-    </div>
+      <Hero />
+    </>
   );
 }
---
+++ component/section/home/hero.tsx
@@ -0,0 +1,36 @@
+// Codes by mahdi tasha
+// Importing part
+import { Button } from "@/component/ui/button";
+import { ChartColumnIncreasing, LayoutDashboard } from "lucide-react";
+import Link from "next/link";
+
+// Creating and exporting Hero section of home page as default
+export default function Hero() {
+  // Returning JSX
+  return (
+    <section className="max-w-4xl border-x border-x-foreground/10 mx-auto">
+      <main className="prose dark:prose-invert prose-neutral min-h-[calc(100dvh-65px)] p-4">
+        <h1 className="mt-0">Build Systems. Grow Daily.</h1>
+        <p>
+          EvolveOS is a markdown-first, offline-friendly personal growth OS that
+          helps you track habits, journal daily, plan tasks, and manage your
+          life — without the complexity of Notion.
+        </p>
+        <div className="flex items-center justify-start gap-3 flex-wrap">
+          <Button asChild size="lg">
+            <Link href="/dashboard" className="no-underline">
+              <LayoutDashboard />
+              Get Started
+            </Link>
+          </Button>
+          <Button asChild size="lg" variant={"outline"}>
+            <Link href="/#learn" className="no-underline">
+              <ChartColumnIncreasing />
+              Learn More
+            </Link>
+          </Button>
+        </div>
+      </main>
+    </section>
+  );
+}
