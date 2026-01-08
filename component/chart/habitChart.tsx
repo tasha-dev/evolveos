88e1dcbdfbb966ba3458ec6d0ca2c8eb99194bc3
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2026-01-08 13:23:17 +0330

    removed unensecory importing

--- component/chart/habitChart.tsx
+++ component/chart/habitChart.tsx
@@ -14,7 +14,6 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/component/ui/chart";
-import moment from "moment";
 import { getLast30DaysHabitPoints } from "@/lib/util";
 import { HabitPoint } from "@/type/util";
 
